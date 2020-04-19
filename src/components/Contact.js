3b8ecb7cdf5e8b0912cce8ad8d285a588621c9d1
Anneline <anneline.guttormsen@hotmail.no>
2020-04-19 06:49:58 +0200

    netlify fiks

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -24,21 +24,49 @@ export default function Contact() {
         setState({formData: data});
     }
 
+    console.log(state.formData == "");
+    
+    let formHTML;
+    if (state.formData == "") {
+        formHTML = (
+        <>
+        <h2><span className="span__wrap"><span className="span__wrap__text">Kontakt meg!</span></span></h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <label><p>Navn</p></label>
+            <input 
+                type="text" 
+                placeholder="Ole Nordmann" 
+                name="name" 
+                ref={register({required:true})}/>
+            {errors.name && <ErrorMessage content={errors.name.message}/>}
+            <label><p>E-post</p></label>
+            <input 
+                type="text" 
+                placeholder="ole.nordmann@gmail.com" 
+                name="email" 
+                ref={register({required:true})}/>
+            {errors.email && <ErrorMessage content={errors.email.message}/>}
+            <label><p>Melding</p></label>
+            <textarea 
+                placeholder="Din melding her" 
+                name="message" 
+                ref={register({required:true})}/>
+            {errors.message && <ErrorMessage content={errors.message.message}/>}
+            <button className="btn" type="submit"><p>Send melding</p></button>
+        </form>
+        </>
+    );
+    } else {
+        formHTML = 
+        <div className="contact__response">
+            <h2><span className="span__wrap"><span className="span__wrap__text">Takk for din melding!</span></span></h2>
+            <p>Videre kontakt vil skje via e-post</p>
+        </div>
+    }
+
     return (
         <div className="col-12 mainelement contact">
-            <h2><span className="span__wrap"><span className="span__wrap__text">Kontakt meg!</span></span></h2>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <label><p>Navn</p></label>
-                <input type="text" placeholder="Ole Nordmann" name="name" ref={register({required:true})}/>
-                {errors.name && <ErrorMessage content={errors.name.message}/>}
-                <label><p>E-post</p></label>
-                <input type="text" placeholder="ole.nordmann@gmail.com" name="email" ref={register({required:true})}/>
-                {errors.email && <ErrorMessage content={errors.email.message}/>}
-                <label><p>Melding</p></label>
-                <textarea placeholder="Din melding her" name="message" ref={register({required:true})}></textarea>
-                {errors.message && <ErrorMessage content={errors.message.message}/>}
-                <button className="btn" type="submit"><p>Send melding</p></button>
-            </form>
+            {formHTML}
         </div>
     )
 }
